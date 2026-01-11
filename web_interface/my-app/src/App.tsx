59a04b4c564b7d32a8322a4eb34bfaec704a354c
Geoff sdfgeoff <sdfgeoff@gmail.com>
2026-01-11 16:51:34 +1300

    Remove fluff

--- web_interface/my-app/src/App.tsx
+++ web_interface/my-app/src/App.tsx
@@ -1,11 +1,8 @@
 import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useWebSocket } from './useWebSocket'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [lastMessage, setLastMessage] = useState<string>('')
   const [outgoingMessage, setOutgoingMessage] = useState('')
 
@@ -37,15 +34,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <p>
           WebSocket: <strong>{wsStatus}</strong>
@@ -64,16 +52,7 @@ function App() {
             Send
           </button>
         </div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
